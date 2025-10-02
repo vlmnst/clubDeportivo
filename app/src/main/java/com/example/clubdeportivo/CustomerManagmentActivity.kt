a062bc3df098d22e35f5b85dc400cd4514adc37a
Valeria Mansueto <vale-mansueto@hotmail.com>
2025-10-02 20:48:34 -0300

    feat: agrego selectores

--- app/src/main/java/com/example/clubdeportivo/CustomerManagmentActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/CustomerManagmentActivity.kt
@@ -11,6 +11,19 @@ class CustomerManagmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_customer_managment)
+
+        // 1. Encuentra cada selector por su ID
+        val selectorCliente = findViewById<SelectorView>(R.id.selector_cliente)
+        val selectorCarnet = findViewById<SelectorView>(R.id.selector_carnet)
+
+        selectorCliente.setLabel("Filtrar por cliente:")
+        val opcionesCliente = listOf("Todos", "Socio", "No Socio")
+        selectorCliente.setOptions(opcionesCliente)
+
+        selectorCarnet.setLabel("Filtrar por carnet:")
+        val opcionesCarnet = listOf("Todos", "Con carnet", "Sin Carnet")
+        selectorCarnet.setOptions(opcionesCarnet)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/clubdeportivo/MenuPrincipal.kt
+++ app/src/main/java/com/example/clubdeportivo/MenuPrincipal.kt
@@ -1,6 +1,8 @@
 package com.example.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MenuPrincipal : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnManageCustomer = findViewById<Button>(R.id.btn_manage_customer)
+        // listen click == addEventListener('click')
+        btnManageCustomer.setOnClickListener {
+            val intent = Intent(this, CustomerManagmentActivity::class.java)
+            startActivity(intent)
         }
     }
 }
